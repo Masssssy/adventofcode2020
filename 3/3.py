7c87471df19b28d5255683f43e6a3f76ca2f8a46
Martin Fredriksson <martin.fredriksson@tangiamo.com>
2020-12-03 12:58:39 +0100

    Cleanup on aisle 3

--- 3/3.py
+++ 3/3.py
@@ -1,33 +1,22 @@
 input = [line.rstrip('\n') for line in open("input")]
 
-xPos = 0
-yPos = 0
-
-def takeStep(x,y, rowLen):
-    global xPos
-    global yPos
-    xPos += x
-    yPos += y
-    xPos = xPos % rowLen
-    return [x,y]
-
 rows = len(input)
 rowLen = len(input[0])
 
 def calcTrees(slopeX, slopeY):
-    global xPos
-    global yPos
-    xPos = 0
-    yPos = 0
+    pos = [0,0]
     
     trees = 0
-    for b in range(1000000):
-        takeStep(slopeX,slopeY, rowLen)
-        if(yPos > rows-1):
+    for b in range(rows):
+        pos = takeStep(slopeX,slopeY, pos[0], pos[1],rowLen)
+        if(pos[1] > rows-1):
             break
-        elif(input[yPos][xPos] == "#"):
+        elif(input[pos[1]][pos[0]] == "#"):
             trees += 1
     return trees
+
+def takeStep(x,y, startX, startY, rowLen):
+    return [(startX + x) % rowLen, startY + y]
 
 print(calcTrees(3,1))
 print(calcTrees(1,1)*calcTrees(3,1)*calcTrees(5,1)*calcTrees(7,1)*calcTrees(1,2))
